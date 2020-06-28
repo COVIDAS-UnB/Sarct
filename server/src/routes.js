72ac51323dd5cdeb31da1fe85016452881d93bc2
richardjlv <richardjunio39@gmail.com>
2020-06-28 18:32:10 -0300

    Adding login

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   res.render('index.html');
 });
+
+routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
