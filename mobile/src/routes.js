027cb0f02bdfaa1f51a1c66ba3aae3167478d3d2
richardjlv <richardjunio39@gmail.com>
2020-06-28 00:34:41 -0300

    Initial mobile structure

---
+++ mobile/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Routes = () => {
+  return (
+    <View
+      style={{
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+      }}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default Routes;
